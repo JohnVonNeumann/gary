60cf6a6599cb0af6be7ea23b2c0507efbbb49dc6
JohnVonNeumann <louiswillcock@gmail.com>
2019-07-06 16:24:09 +1000

    FIX lint errors - shellcheck

--- find_secrets.sh
+++ find_secrets.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep -re "[0-9a-f]\{6\}" --exclude-dir={.git,vendor} --exclude={*.log,*.log.?} --color=always --line-number $1
+grep -re "[0-9a-f]\{6\}" --exclude-dir={.git,vendor} --exclude={*.log,*.log.?} --color=always --line-number "$1"
